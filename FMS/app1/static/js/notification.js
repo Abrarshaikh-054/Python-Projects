461b08a4589dbd41ca528e62a3f6fc1388f20e6b
Abrar Shaikh <sheikhabrar054@gmail.com>
2024-08-03 23:12:50 +0530

    Delete FMS directory

--- FMS/app1/static/js/notification.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const alert = document.querySelector('.alert');
-    if (alert) {
-        setTimeout(() => {
-            alert.classList.remove('show');
-            alert.classList.add('hide');
-        }, 3000); // Adjust the time (3000ms = 3 seconds) as needed
-    }
-});
